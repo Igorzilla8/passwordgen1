6af1811a18a7341f083504990bba56f64adb401b
Igorzilla8 <36893248+Igorzilla8@users.noreply.github.com>
2018-02-28 00:18:28 +0100

    popravki
    popravki

--- passwordGen.py
+++ passwordGen.py
@@ -1,3 +1,4 @@
+#Make sure that the generated passwords are unique and strong, 
 import sys
 import string
 import random
@@ -6,6 +7,12 @@ minPassLen = 4 			# minimal length of the password
 maxPassLen = 60			# maximal length of the password
 bruteCalcPerformance = 10000000 # performance of brute force cracking 
 
+string.symbols 	= '@#$%' 				# included in collection
+string.ambigous = '{}[]()/\'"`~,;:.<>' 	# excluded from collection
+string.similar 	= 'il1Lo0O' 			# excluded from collection
+string.collection = string.letters + string.lettersC + string.numbers + string.symbols  
+
+# help for user
 def printHelp():
 	print('Proper use of password generator is: ')
 	print('  ',sys.argv[0],' #PasswordLength [-t]')
@@ -17,6 +24,18 @@ def printHelp():
 	print('   be beetween', minPassLen, 'and', maxPassLen)
 	return
 
+# function checks if there exsists any character from string candidates
+# in string passw 
+def containsChar(candidates, passw):	
+	for i in range (0, len(candidates)):
+		if candidates[i] in passw:
+			return True
+	return False
+
+# function checks if password always have a mix of lowercase letters, uppercase letters, numbers, and symbols. 
+def checkForStrongPass(passw):
+ 	return containsChar(string.letters, passw) and containsChar(string.lettersC, passw) and containsChar(string.numbers, passw) and containsChar(string.symbols, passw)
+	
 # input check; syntax check of password length
 try:
 	int(sys.argv[1])
@@ -28,21 +47,16 @@ except:
 if (int(sys.argv[1]) < minPassLen or int(sys.argv[1]) > maxPassLen):
 	print('Password length should be beetween', minPassLen, 'and', maxPassLen, '.')
 	exit(0)
-
-# collection of characters which will be used for creation of the password	
-string.letters 	= 'abcdefghjkmnpqrstuvwxyzABCDEFGHIJKMNPQRSTUVWXYZ' # included in collection
-string.numbers 	= '23456789' 			# included in collection
-string.symbols 	= '@#$%' 				# included in collection
-string.ambigous = '{}[]()/\'"`~,;:.<>' 	# excluded from collection
-string.similar 	= 'il1Lo0O' 			# excluded from collection
-string.collection = string.letters + string.numbers + string.symbols  
-
-# generation of the password
-string.password = ''
-for i in range (0, int(sys.argv[1])):
-	string.password += random.choice(string.collection)
+	
+# generate strong and unique password
+while True:
+	string.password = ''
+	for i in range (0, int(sys.argv[1])):
+		string.password += random.choice(string.collection)
+	if checkForStrongPass(string.password):
+		break
 print('Password:', string.password)
-
+		
 # calculation of time for cracking password lengthOfPassword ^ numberOfPossibleCharacters / 10M
 try:
 	sys.argv[2] 
